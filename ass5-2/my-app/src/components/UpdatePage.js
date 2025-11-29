979f5ee26183b7f22de6d3a262d9ead1a42e602c
jayjhkim <kjhim1001@gmail.com>
2025-11-29 21:06:23 +0900

    f

--- ass5-2/my-app/src/components/UpdatePage.js
+++ ass5-2/my-app/src/components/UpdatePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { API_URL } from "../api";
 
 function UpdatePage() {
